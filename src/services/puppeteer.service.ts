2b599dbb84c8e8cd4165ee2cc290589b65a327ff
Cindy Tran <cindyztran@gmail.com>
2023-07-31 21:24:04 -0700

    feat: check if input element is disabled

--- src/services/puppeteer.service.ts
+++ src/services/puppeteer.service.ts
@@ -6,9 +6,19 @@ const getStock = async () => {
   });
 
   const page = await browser.newPage();
-  await page.goto("https://www.nike.com");
-  await page.screenshot({ path: "example.png" });
-  await browser.close();
+
+  await page.goto(
+    "https://www.nike.com/t/air-monarch-iv-mens-training-shoes-G5Xn1k/415445-102"
+  );
+
+  const id = "#skuAndSize__29304480";
+  const isDisabled = await page.$eval(id, (el: any) => {
+    if (!el) {
+      throw new Error(`No element with id: ${id} found`);
+    }
+    return el.disabled;
+  });
+  console.log({ isDisabled });
 };
 
 export default {
