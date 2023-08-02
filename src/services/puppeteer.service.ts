c0682e13290efa17b8295ba3dcc963ba3a6bae97
Cindy Tran <cindyztran@gmail.com>
2023-08-01 18:58:22 -0700

    feat: send email if in stock

--- src/services/puppeteer.service.ts
+++ src/services/puppeteer.service.ts
@@ -1,24 +1,38 @@
 import puppeteer from "puppeteer";
+import gmailService from "./gmail.service";
 
 const getStock = async () => {
-  const browser = await puppeteer.launch({
-    headless: "new",
-  });
+  try {
+    const browser = await puppeteer.launch({
+      headless: "new",
+    });
 
-  const page = await browser.newPage();
+    const page = await browser.newPage();
 
-  await page.goto(
-    "https://www.nike.com/t/air-monarch-iv-mens-training-shoes-G5Xn1k/415445-102"
-  );
+    const link =
+      "https://www.nike.com/t/air-monarch-iv-mens-training-shoes-G5Xn1k/415445-102";
 
-  const id = "#skuAndSize__29304480";
-  const isDisabled = await page.$eval(id, (el: any) => {
-    if (!el) {
-      throw new Error(`No element with id: ${id} found`);
+    await page.goto(link);
+
+    const id = "#skuAndSize__29304480";
+    const isDisabled = await page.$eval(id, (el: any) => {
+      if (!el) {
+        throw new Error(`No element with id: ${id} found`);
+      }
+      return el.disabled;
+    });
+
+    if (!isDisabled) {
+      await gmailService.sendEmail({
+        from: process.env.SUBJECT_EMAIL_ADDRESS as string,
+        to: process.env.TEST_EMAIL as string,
+        subject: "Nike Air Monarch IV Size Restock!",
+        message: `<h1>Hello, the shoe size has been restocked!</h1><br/><a href=${link}>Go to page</a>`,
+      });
     }
-    return el.disabled;
-  });
-  console.log({ isDisabled });
+  } catch (err: any) {
+    console.log({ err: err.message });
+  }
 };
 
 export default {
