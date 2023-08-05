0d9a0cc2ead82c4c36f460d9a95109ce680e1f9c
Cindy Tran <cindyztran@gmail.com>
2023-08-05 17:59:43 -0500

    fix: update path

--- src/utils/gmail.ts
+++ src/utils/gmail.ts
@@ -1,7 +1,7 @@
 import path from "path";
 import fs from "fs";
 
-export const CREDENTIALS_PATH = path.join(process.cwd(), "sa.json");
+export const CREDENTIALS_PATH = path.join(process.cwd(), "../../sa.json");
 
 export const credentials = JSON.parse(
   fs.readFileSync(CREDENTIALS_PATH, "utf8")
