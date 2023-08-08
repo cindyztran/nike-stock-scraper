2702479db87e3a88a4d67fbe8c1239598fa7307a
Cindy Tran <cindyztran@gmail.com>
2023-08-07 20:58:44 -0500

    chore: remove test endpoints

--- functions/src/routes/index.ts
+++ functions/src/routes/index.ts
@@ -1,12 +1,8 @@
 import { Router as expressRouter } from "express";
 import GeneralRoute from "./general.routes";
-import PuppeteerRoute from "./puppeteer.routes";
-import GmailRoute from "./gmail.routes";
 
 const router = expressRouter();
 
 router.use("/", GeneralRoute);
-router.use("/puppeteer", PuppeteerRoute);
-router.use("/gmail", GmailRoute);
 
 export default router;
