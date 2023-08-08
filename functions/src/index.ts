2723601c96cd673a7a31c2e08e349564f6370c23
Cindy Tran <cindyztran@gmail.com>
2023-08-07 22:35:51 -0500

    update: functions v2

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,15 +1,12 @@
-import { HttpsFunction, Request, Response } from "firebase-functions";
-import * as functions from "firebase-functions";
+import * as functions from "firebase-functions/v2/https";
 import puppeteerService from "./services/puppeteer.service";
 
-export const puppeteer: HttpsFunction = functions.https.onRequest(
-  async (req: Request, res: Response) => {
-    try {
-      await puppeteerService.getStock();
-      res.send("Success");
-    } catch (err: any) {
-      console.error(err);
-      res.status(500).send("An error occurred");
-    }
+export const puppeteer = functions.onRequest(async (req: any, res: any) => {
+  try {
+    await puppeteerService.getStock();
+    res.send("Success");
+  } catch (err: any) {
+    console.error(err);
+    res.status(500).send("An error occurred");
   }
-);
+});
