f86665fb3c54f1ad2a78908ef04513b9dccb51bd
Cindy Tran <cindyztran@gmail.com>
2023-08-07 23:03:54 -0500

    update: memory and timeout

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,12 +1,18 @@
 import * as functions from "firebase-functions/v2/https";
 import puppeteerService from "./services/puppeteer.service";
 
-export const puppeteer = functions.onRequest(async (req: any, res: any) => {
-  try {
-    await puppeteerService.getStock();
-    res.send("Success");
-  } catch (err: any) {
-    console.error(err);
-    res.status(500).send("An error occurred");
+export const puppeteer = functions.onRequest(
+  {
+    memory: "2GiB",
+    timeoutSeconds: 3600,
+  },
+  async (req: any, res: any) => {
+    try {
+      await puppeteerService.getStock();
+      res.send("Success");
+    } catch (err: any) {
+      console.error(err);
+      res.status(500).send("An error occurred");
+    }
   }
-});
+);
